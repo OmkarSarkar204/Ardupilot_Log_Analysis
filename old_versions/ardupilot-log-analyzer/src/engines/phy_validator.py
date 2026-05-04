5ea6a4afaf0637fc0cfbd2a29fbcb1544a909908
Omkar Sarkar <omkarsarkar24@gmail.com>
2026-05-04 19:55:31 +0530

    Cleaning
    Signed-off-by: Omkar Sarkar <omkarsarkar24@gmail.com>

--- old_versions/ardupilot-log-analyzer/src/engines/phy_validator.py
+++
@@ -1,9 +0,0 @@
-def physics_validator(motor_spread, roll_error, volt_sag, power_watts, spread_thresh, roll_thresh, sag_thresh):
-    '''Dynamic physical limits based on specific flight baselines'''
-    if motor_spread > spread_thresh and roll_error > roll_thresh:
-        return 1
-    
-    if volt_sag > sag_thresh:
-        return 2
-        
-    return 0
